abacd830c978f6dea47a7bc4fd6796a1874d09bb
marcosbeirigo <marcosbeirigo@gmail.com>
2011-05-30 16:40:52 -0400

    remoção de código não utilizado

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,3 @@
 module ApplicationHelper
-  def pdf_image_tag(image, options = {})
-    options[:src] = File.expand_path(RAILS_ROOT) + '/public/images/' + image 
-    tag(:img, options)
-  end
 
 end
